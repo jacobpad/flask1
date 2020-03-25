d98b414266287efa884515500082631d62416833
jacobpad <jacobpad@gmail.com>
2020-03-25 13:43:19 -0700

    Added first templates and simple html

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
+# FLASK_APP=hello.py flask run
 """MINIMAL FLASK APP"""
 
-from flask import Flask
+from flask import Flask, render_template
+# render_template() automatically looks for a folder called templates to find the files in the decorators
 
 # Make the application
 app = Flask(__name__)
 
-# Make the rout
+# Make the rout and function for home page
 @app.route("/")
-
 def hello():
-    return "Hello!"
+    return render_template('home.html')
+
+# Make out and functionn for about page
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
+    
