6f2ffa8f5b2575f73e598338895d37d652957494
jacobpad <jacobpad@gmail.com>
2020-03-25 14:25:30 -0700

    modified app to see database

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 # render_template() automatically looks for a folder called templates to find the files in the decorators
+from .models import DB
+
 
 def create_app():
     app = Flask(__name__)
 
-    @app.rout('/')
+    # Add a config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+    # Connect the database and app
+    DB.init_app(app)
+
+    @app.route('/')
     def root():
         return "Welcome Home"
 
---
+++ TWITOFF/models.py
@@ -0,0 +1,19 @@
+""" Database models """
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+# Import database (CAPS for global scope)
+DB = SQLAlchemy()
+
+class User(DB.Model):
+    """ Twitter users that we analize """
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+
+class Tweet(DB.Model):
+    """ The users tweets from twitter """
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.Unicode(280))
+    
