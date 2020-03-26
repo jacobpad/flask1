c470188b05b74128762887b647440a0667747bc6
jacobpad <jacobpad@gmail.com>
2020-03-25 23:51:15 -0700

    forgot to save

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -48,5 +48,28 @@ def create_app():
         DB.create_all()
         return render_template('base.html', title='Rest Database', users=[])
 
+        # 
+        # Add to database
+        # 
+        # FLASK_APP=TWITOFF:APP flask shell
+        # from TWITOFF.models import *
+        # DB.drop_all()
+        # DB.create_all()
+        # twitter_user = TWITTER.get_user('DatingDivas')
+        # tweets = twitter_user.timeline(count=200, exclude_replies=True, include_rts=False, tweet_mode='extended')
+        # db_user=User(id=twitter_user.id, name=twitter_user.screen_name, newest_tweet_id=tweets[0].id)
+        # 
+        # >>> for tweet in tweets:
+        # ...   embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+        # ...   db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500], embedding=embedding)
+        # ...   DB.session.add(db_tweet)
+        # ...   db_user.tweets.append(db_tweet)
+        # 
+        # DB.session.add(db_user)
+        # DB.session.commit()
+        # quit()
+        # 
+        # EXPLAINED: https://youtu.be/2YE6jcy-chg?t=7007
+
 
     return app
